3ede0c5808832e95546d6a24eb6e6267e8a989b3
Il-seob Bae <iluvtrollhd@gmail.com>
2021-12-21 21:13:57 +0900

    Add empty spring boot project

---
+++ src/main/java/me/codinginterview/techinterviewserver/TechInterviewServerApplication.java
@@ -0,0 +1,13 @@
+package me.codinginterview.techinterviewserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechInterviewServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechInterviewServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/codinginterview/techinterviewserver/TechInterviewServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.codinginterview.techinterviewserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechInterviewServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
