00e384646b8076fac6ec958daeb195e66a2a83e0
Il-seob Bae <iluvtrollhd@gmail.com>
2021-12-21 22:21:17 +0900

    Add entities

---
+++ src/main/java/me/codinginterview/techinterviewserver/infra/entity/Post.java
@@ -0,0 +1,24 @@
+package me.codinginterview.techinterviewserver.infra.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue
+    Long id;
+    String title;
+    @Lob
+    String body;
+    Date created;
+    @ManyToOne
+    User owner;
+    @OneToMany(mappedBy = "post")
+    List<Comment> comments;
+    @ManyToMany(mappedBy = "bookmarks")
+    List<User> bookmarkedUsers;
+    @ManyToMany(mappedBy = "likes")
+    List<User> likedUsers;
+}
